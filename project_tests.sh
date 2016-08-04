6ab572c99fbca7fe16247ab6f12fddd22d7b531f
jennifer <jenniferstrej@gmail.com>
2016-08-04 18:00:14 +0100

    fix link origin and mac incompatible option on ln command

--- project_tests.sh
+++ project_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source venv/bin/activate
 pip install -r requirements.txt
-ln -sfT settings.py settings_test.py
+ln -sf dashboard/dev_settings_test.py settings_test.py
 rm -rf build/junit.xml
 CONFIG_FILE="test" python -m pytest --junitxml build/junit.xml dashboard/
