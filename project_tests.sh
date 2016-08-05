1df912e13522ea6adc1879516666581796cbabe0
Giorgio Sironi <g.sironi@elifesciences.org>
2016-08-05 14:20:22 +0100

    Tests now use the settings.py provided by Builder

--- project_tests.sh
+++ project_tests.sh
@@ -3,4 +3,4 @@ source venv/bin/activate
 pip install -r requirements.txt
 ln -sfT settings.py settings_test.py
 rm -rf build/junit.xml
-CONFIG_FILE="test" python -m pytest --junitxml build/junit.xml dashboard/
+python -m pytest --junitxml build/junit.xml dashboard/
