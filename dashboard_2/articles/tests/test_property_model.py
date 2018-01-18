9939343e41ff8f8cd9a1255f3240b5a48d92e078
Sean Wiseman <seanwiseman2012@gmail.com>
2018-01-18 15:12:53 +0000

    Removed (now) unnecessary Property exclusion test

--- dashboard_2/articles/tests/test_property_model.py
+++ dashboard_2/articles/tests/test_property_model.py
@@ -53,26 +53,3 @@ def test_can_get_latest_article_ids_excluding_hidden_articles(article, propertie
 
 	# make sure 'already published' article id is not present
 	assert article_2.article_identifier not in latest_article_ids
-
-
-@pytest.mark.django_db
-def test_can_get_latest_article_ids_without_wrongfully_exluding(article, properties_v1):
-	"""Should not exclude properties with 'Property.find.HIDDEDN_STATUSES' if
-	the Property `name` is not 'publication-status'
-	"""
-
-	article_2 = Article.objects.create(article_identifier='01234')
-
-	# has the value of `hidden` but not the name of `publication-status`
-	Property.objects.create(article_id=article_2.article_id,
-	                        name='some-name',
-	                        text_value='hidden',
-	                        property_type='text',
-	                        version=2)
-
-	# get latest article identifiers
-	latest_article_ids = Property.find.latest_articles()
-
-	# make sure article with 'hidden' property is still present as
-	# the name value != 'publication-status'
-	assert article_2.article_identifier in latest_article_ids
