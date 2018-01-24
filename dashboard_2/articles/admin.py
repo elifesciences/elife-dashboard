ffc8a8061979a5152ee2d8137ad80729d01969dc
Sean Wiseman <seanwiseman2012@gmail.com>
2018-01-24 14:11:47 +0000

    expanded admin models

--- dashboard_2/articles/admin.py
+++ dashboard_2/articles/admin.py
@@ -8,12 +8,52 @@ from .models import (
 )
 
 
+class ArticleAdmin(admin.ModelAdmin):
+	model = Article
+	list_display = ('article_id', 'article_identifier')
+	search_fields = ('article_id', 'article_identifier')
+
+
+class EventAdmin(admin.ModelAdmin):
+	model = Event
+	list_display = (
+		'event_id',
+		'article',
+		'type',
+		'version',
+		'run',
+		'status',
+		'timestamp',
+		'message'
+	)
+	search_fields = (
+		'event_id',
+		'type',
+		'status',
+		'timestamp',
+		'message'
+	)
+
+
+class MessageAdmin(admin.ModelAdmin):
+	model = Message
+	list_display = ('message_id', 'timestamp')
+	search_fields = ('message_id', 'timestamp')
+
+
 class PropertyAdmin(admin.ModelAdmin):
-    model = Property
-    search_fields = ('property_id', 'name')
+	model = Property
+	list_display = (
+		'property_id',
+		'article',
+		'name',
+		'version',
+		'property_type'
+	)
+	search_fields = ('property_id', 'name', 'property_type')
 
 
-admin.site.register(Article)
-admin.site.register(Event)
-admin.site.register(Message)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Event, EventAdmin)
+admin.site.register(Message, MessageAdmin)
 admin.site.register(Property, PropertyAdmin)
