0cdb0e3d4b03306155933172264a6a1cd4117d20
Sean Wiseman <seanwiseman2012@gmail.com>
2018-01-25 10:15:12 +0000

    fixtures now have type hints

--- dashboard_2/articles/tests/conftest.py
+++ dashboard_2/articles/tests/conftest.py
@@ -1,6 +1,7 @@
 import json
 import os
 import time
+from typing import Dict, List
 
 import pytest
 from rest_framework.test import APIClient
@@ -14,29 +15,31 @@ from articles.models import (
 
 
 @pytest.fixture
-def json_data_path():
+def json_data_path() -> str:
     return os.path.join(os.path.dirname(os.path.abspath(__file__)), 'json')
 
 
 @pytest.fixture
-def api_client():
+def api_client() -> APIClient:
     return APIClient()
 
 
 @pytest.fixture
 @pytest.mark.django_db
-def article():
+def article() -> Article:
     return Article.objects.create(article_identifier='09003')
 
 
 @pytest.fixture
 @pytest.mark.django_db
-def article_complete(article, events_for_09003, properties_v1, properties_v2):
+def article_complete(article: Article, events_for_09003: List[Event],
+                     properties_v1: List[Property],
+                     properties_v2: List[Property]) -> Article:
     return article
 
 
 @pytest.fixture
-def article_detail_response_data(json_data_path):
+def article_detail_response_data(json_data_path: str) -> Dict:
     file_name = 'article_detail_response.json'
 
     with open(os.path.join(json_data_path, file_name), 'r') as json_file:
@@ -46,7 +49,7 @@ def article_detail_response_data(json_data_path):
 
 
 @pytest.fixture
-def article_mutli_version_response_data(json_data_path):
+def article_mutli_version_response_data(json_data_path: str) -> Dict:
     file_name = 'article_multi_version_response.json'
 
     with open(os.path.join(json_data_path, file_name), 'r') as json_file:
@@ -57,7 +60,7 @@ def article_mutli_version_response_data(json_data_path):
 
 @pytest.fixture
 @pytest.mark.django_db
-def event_convert_jats_v1(article):
+def event_convert_jats_v1(article: Article) -> Event:
     return Event.objects.create(version=1,
                                 run='b6ef5d1f-23b3-4f4e-9ba3-7de24f885171',
                                 type='Convert JATS',
@@ -66,16 +69,17 @@ def event_convert_jats_v1(article):
                                 message='Starting conversion of article xml to EIF for 09003',
                                 article_id=article.article_id)
 
+
 @pytest.fixture
 @pytest.mark.django_db
-def message():
+def message() -> Message:
     return Message.objects.create(message_id='2a4ee336-288a-47e2-9d74-1b337642df57',
                                   timestamp='2016-03-23 12:15:22.225078')
 
 
 @pytest.fixture
 @pytest.mark.django_db
-def property_article_type_v1(article):
+def property_article_type_v1(article: Article) -> Property:
     return Property.objects.create(article_id=article.article_id,
                                    name='article-type',
                                    text_value='research-article',
@@ -85,7 +89,7 @@ def property_article_type_v1(article):
 
 @pytest.fixture
 @pytest.mark.django_db
-def property_authors_v1(article):
+def property_authors_v1(article: Article) -> Property:
     return Property.objects.create(article_id=article.article_id,
                                    name='authors',
                                    text_value='Anthony D Fouad, Shelly Teng, Julian R Mark',
@@ -95,7 +99,7 @@ def property_authors_v1(article):
 
 @pytest.fixture
 @pytest.mark.django_db
-def property_corresponding_authors_v1(article):
+def property_corresponding_authors_v1(article: Article) -> Property:
     return Property.objects.create(article_id=article.article_id,
                                    name='corresponding-authors',
                                    text_value='Christopher Fang-Yen',
@@ -105,7 +109,7 @@ def property_corresponding_authors_v1(article):
 
 @pytest.fixture
 @pytest.mark.django_db
-def property_doi_v1(article):
+def property_doi_v1(article: Article) -> Property:
     return Property.objects.create(article_id=article.article_id,
                                    name='doi',
                                    text_value='10.7554/eLife.09003',
@@ -115,7 +119,7 @@ def property_doi_v1(article):
 
 @pytest.fixture
 @pytest.mark.django_db
-def property_path_v1(article):
+def property_path_v1(article: Article) -> Property:
     return Property.objects.create(article_id=article.article_id,
                                    name='path',
                                    text_value='content/7/e33511v1',
@@ -125,7 +129,7 @@ def property_path_v1(article):
 
 @pytest.fixture
 @pytest.mark.django_db
-def property_path_v2(article):
+def property_path_v2(article: Article) -> Property:
     return Property.objects.create(article_id=article.article_id,
                                    name='path',
                                    text_value='content/7/e33511v1',
@@ -135,7 +139,7 @@ def property_path_v2(article):
 
 @pytest.fixture
 @pytest.mark.django_db
-def property_publication_date_v1(article):
+def property_publication_date_v1(article: Article) -> Property:
     return Property.objects.create(article_id=article.article_id,
                                    name='publication-date',
                                    text_value='2018-01-16',
@@ -145,7 +149,7 @@ def property_publication_date_v1(article):
 
 @pytest.fixture
 @pytest.mark.django_db
-def property_publication_status_v1(article):
+def property_publication_status_v1(article: Article) -> Property:
     return Property.objects.create(article_id=article.article_id,
                                    name='publication-status',
                                    text_value='ready to publish',
@@ -155,7 +159,7 @@ def property_publication_status_v1(article):
 
 @pytest.fixture
 @pytest.mark.django_db
-def property_status_v1(article):
+def property_status_v1(article: Article) -> Property:
     return Property.objects.create(article_id=article.article_id,
                                    name='status',
                                    text_value='VOR',
@@ -165,7 +169,7 @@ def property_status_v1(article):
 
 @pytest.fixture
 @pytest.mark.django_db
-def property_title_v1(article):
+def property_title_v1(article: Article) -> Property:
     return Property.objects.create(article_id=article.article_id,
                                    name='title',
                                    text_value='Distributed rhythm generators and Caenorhabditis',
@@ -175,10 +179,10 @@ def property_title_v1(article):
 
 @pytest.fixture
 @pytest.mark.django_db
-def properties_v1(property_article_type_v1, property_authors_v1,
-                  property_corresponding_authors_v1, property_doi_v1,
-                  property_publication_date_v1, property_publication_status_v1,
-                  property_status_v1, property_title_v1):
+def properties_v1(property_article_type_v1: Property, property_authors_v1: Property,
+                  property_corresponding_authors_v1: Property, property_doi_v1: Property,
+                  property_publication_date_v1: Property, property_publication_status_v1: Property,
+                  property_status_v1: Property, property_title_v1: Property) -> List[Property]:
 
     return [property_article_type_v1, property_authors_v1,
             property_corresponding_authors_v1, property_doi_v1,
@@ -188,7 +192,7 @@ def properties_v1(property_article_type_v1, property_authors_v1,
 
 @pytest.fixture
 @pytest.mark.django_db
-def property_article_type_v2(article):
+def property_article_type_v2(article: Article) -> Property:
     return Property.objects.create(article_id=article.article_id,
                                    name='article-type',
                                    text_value='research-article',
@@ -198,7 +202,7 @@ def property_article_type_v2(article):
 
 @pytest.fixture
 @pytest.mark.django_db
-def property_authors_v2(article):
+def property_authors_v2(article: Article) -> Property:
     return Property.objects.create(article_id=article.article_id,
                                    name='authors',
                                    text_value='Anthony D Fouad, Shelly Teng, Julian R Mark',
@@ -208,7 +212,7 @@ def property_authors_v2(article):
 
 @pytest.fixture
 @pytest.mark.django_db
-def property_corresponding_authors_v2(article):
+def property_corresponding_authors_v2(article: Article) -> Property:
     return Property.objects.create(article_id=article.article_id,
                                    name='corresponding-authors',
                                    text_value='Christopher Fang-Yen',
@@ -218,7 +222,7 @@ def property_corresponding_authors_v2(article):
 
 @pytest.fixture
 @pytest.mark.django_db
-def property_doi_v2(article):
+def property_doi_v2(article: Article) -> Property:
     return Property.objects.create(article_id=article.article_id,
                                    name='doi',
                                    text_value='10.7554/eLife.09003',
@@ -228,7 +232,7 @@ def property_doi_v2(article):
 
 @pytest.fixture
 @pytest.mark.django_db
-def property_publication_date_v2(article):
+def property_publication_date_v2(article: Article) -> Property:
     return Property.objects.create(article_id=article.article_id,
                                    name='publication-date',
                                    text_value='2018-01-16',
@@ -238,7 +242,7 @@ def property_publication_date_v2(article):
 
 @pytest.fixture
 @pytest.mark.django_db
-def property_publication_status_v2(article):
+def property_publication_status_v2(article: Article) -> Property:
     return Property.objects.create(article_id=article.article_id,
                                    name='publication-status',
                                    text_value='ready to publish',
@@ -248,7 +252,7 @@ def property_publication_status_v2(article):
 
 @pytest.fixture
 @pytest.mark.django_db
-def property_status_v2(article):
+def property_status_v2(article: Article) -> Property:
     return Property.objects.create(article_id=article.article_id,
                                    name='status',
                                    text_value='VOR',
@@ -258,7 +262,7 @@ def property_status_v2(article):
 
 @pytest.fixture
 @pytest.mark.django_db
-def property_title_v2(article):
+def property_title_v2(article: Article) -> Property:
     return Property.objects.create(article_id=article.article_id,
                                    name='title',
                                    text_value='Distributed rhythm generators and Caenorhabditis',
@@ -268,10 +272,10 @@ def property_title_v2(article):
 
 @pytest.fixture
 @pytest.mark.django_db
-def properties_v2(property_article_type_v2, property_authors_v2,
-                  property_corresponding_authors_v2, property_doi_v2,
-                  property_publication_date_v2, property_publication_status_v2,
-                  property_status_v2, property_title_v2):
+def properties_v2(property_article_type_v2: Property, property_authors_v2: Property,
+                  property_corresponding_authors_v2: Property, property_doi_v2: Property,
+                  property_publication_date_v2: Property, property_publication_status_v2: Property,
+                  property_status_v2: Property, property_title_v2: Property) -> List[Property]:
 
     return [property_article_type_v2, property_authors_v2,
             property_corresponding_authors_v2, property_doi_v2,
@@ -280,7 +284,7 @@ def properties_v2(property_article_type_v2, property_authors_v2,
 
 
 @pytest.fixture
-def events_for_09003(article):
+def events_for_09003(article: Article) -> List[Event]:
     event_data = [
         {
             "event-message": "Finished Version Lookup for article 09003 version: 1",
@@ -332,19 +336,21 @@ def events_for_09003(article):
         }
     ]
 
+    events = []
     for event in event_data:
-        Event.objects.create(version=event['version'],
-                             run=event['run-id'],
-                             type=event['event-type'],
-                             timestamp=time.strftime("%Y-%m-%d %H:%M:%S",
-                                                     time.localtime(event['event-timestamp'])),  # 1515150089
-                             status=event['event-status'],
-                             message=event['event-message'],
-                             article_id=article.article_id)
+        obj = Event.objects.create(version=event['version'],
+                                   run=event['run-id'],
+                                   type=event['event-type'],
+                                   timestamp=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(event['event-timestamp'])),  # 1515150089
+                                   status=event['event-status'],
+                                   message=event['event-message'],
+                                   article_id=article.article_id)
+        events.append(obj)
+    return events
 
 
 @pytest.fixture
-def runs_event_data():
+def runs_event_data() -> Dict:
     return {
         "1": {
             "runs": {
