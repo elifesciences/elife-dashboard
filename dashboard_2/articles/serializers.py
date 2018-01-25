e4499bd9244a8084c0f7f4403612eeaa1aaee5f9
Sean Wiseman <seanwiseman2012@gmail.com>
2018-01-25 09:56:17 +0000

    Added ArticleScheduledStatusSerializer

--- dashboard_2/articles/serializers.py
+++ dashboard_2/articles/serializers.py
@@ -5,3 +5,7 @@ class ArticlePublicationStatusSerializer(serializers.Serializer):
     id = serializers.CharField(max_length=512)
     run = serializers.IntegerField()
     version = serializers.IntegerField()
+
+
+class ArticleScheduledstatusSerializer(serializers.Serializer):
+    id = serializers.CharField(max_length=512)
--- dashboard_2/articles/tests/test_serializers.py
+++ dashboard_2/articles/tests/test_serializers.py
@@ -1,6 +1,9 @@
 import pytest
 
-from articles.serializers import ArticlePublicationStatusSerializer
+from articles.serializers import (
+	ArticlePublicationStatusSerializer,
+	ArticleScheduledstatusSerializer,
+)
 
 
 def test_can_validate_pub_status_request_data():
@@ -27,3 +30,19 @@ def test_can_validate_multiple_pub_status_targets():
 def test_will_not_validate_incorrect_pub_status_data_formats(data):
 	serializer = ArticlePublicationStatusSerializer(data=data)
 	assert not serializer.is_valid()
+
+
+def test_can_validate_scheduled_status_request_data():
+	data = {'id': '1234'}
+	serializer = ArticleScheduledstatusSerializer(data=data)
+	assert serializer.is_valid()
+
+
+def test_can_validate_multiple_scheduled_status_targets():
+	data = [
+		{'id': '1234'},
+		{'id': '5678'},
+		{'id': '9999'},
+	]
+	serializer = ArticleScheduledstatusSerializer(data=data, many=True)
+	assert serializer.is_valid()
