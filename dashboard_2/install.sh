7da7f9193f5dc07800e860a7d04bc479ad5be949
Sean Wiseman <seanwiseman2012@gmail.com>
2018-01-29 09:24:38 +0000

    Added install.sh

---
+++ dashboard_2/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+python=/usr/bin/python3.5
+py=${python##*/} # ll: python3.5
+
+# check for exact version of python3
+if [ ! -e "venv/bin/$py" ]; then
+    echo "could not find venv/bin/$py, recreating venv"
+    rm -rf venv
+    $python -m venv venv
+fi
+
+source venv/bin/activate
+
+pip install -r requirements_dashboard_2.txt
+
+
