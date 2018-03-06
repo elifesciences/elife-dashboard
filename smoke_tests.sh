775ed99fb53f4e92caeb33eec0a8a18e1b3788ac
Sean Wiseman <seanwiseman2012@gmail.com>
2018-03-06 11:46:08 +0000

    Changed smoke test to target https

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
 
-[ $(curl --write-out %{http_code} --silent --output /dev/null http://$(hostname):8081/api/current) == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null https://$(hostname):8081/api/current) == 200 ]
