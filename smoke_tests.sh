675e6a5c45c34ce84660f9fb1c1b6643d1c39301
Giorgio Sironi <g.sironi@elifesciences.org>
2019-03-15 14:26:54 +0000

    Smoke test article scheduler
    Removes test for dashboard_2 which has never been used, add one for elife-article-scheduler

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -ex
 
-[ $(curl --write-out %{http_code} --silent --output /dev/null https://$(hostname):8081/api/current) == 200 ]
+# elife-article-scheduler
+[ $(curl --write-out %{http_code} --silent --output /dev/null http://localhost:8080) == 200 ]
