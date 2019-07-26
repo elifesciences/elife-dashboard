13b71456bb499bc4a14e173ce61a1d0abd954f6c
Luke <lsh-0@users.noreply.github.com>
2019-07-26 16:02:08 +0930

    install.sh now uses .lock file and mkvenv.sh to pick a python3 version project_tests.sh now uses install.sh to install requirements

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e # everything must succeed.
-if [ ! -d venv ]; then
-    virtualenv --python=`which python2` venv
-fi
+. mkvenv.sh
 source venv/bin/activate
-pip install -r requirements.txt
+pip install -r requirements.lock
 NEW_RELIC_EXTENSIONS=false pip install --no-binary :all: newrelic==2.82.0.62
--- project_tests.sh
+++ project_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-source venv/bin/activate
-pip install -r requirements.txt
+./install.sh
 ln -sf dashboard/dev_settings_test.py settings_test.py
 rm -rf build/junit.xml
+source venv/bin/activate
 python -m pytest --junitxml build/junit.xml dashboard/
 node_modules/grunt/bin/grunt test
