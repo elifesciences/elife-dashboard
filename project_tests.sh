99f68aac04e63192e7abc087d8ba3e05d1d7243c
Luke <lsh-0@users.noreply.github.com>
2022-08-22 17:10:59 +0930

    bumps js dependencies adds TZ envvar hack to project test runner in case you're not in UTC

--- project_tests.sh
+++ project_tests.sh
@@ -8,4 +8,6 @@ rm -rf build/junit.xml
 source venv/bin/activate
 CONFIG_FILE=test python -m pytest --junitxml build/junit.xml dashboard/
 # lsh@2022-04-01: added '--force' flag to ignore warnings until someone can fix the warnings.
-node_modules/.bin/grunt ci --force
+# lsh@2022-08-22: added 'TZ=UTC' to force headless browser in to UTC. tests with naive datetimes now pass.
+# broke when 'pickadate' upgraded to 3.5.6. change introduced in 3.5.5 (patch version)
+TZ=UTC node_modules/.bin/grunt ci --force
