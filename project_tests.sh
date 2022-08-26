3b7ce7570ee632ef4e321d2bc14a94caa4fdeb4a
Luke <lsh-0@users.noreply.github.com>
2022-08-26 14:28:59 +0930

    removes test.sh, adds .lint.sh, adds .lint.sh to project_tests.sh

--- project_tests.sh
+++ project_tests.sh
@@ -6,6 +6,7 @@ if [ ! -e settings_test.py ]; then
 fi
 rm -rf build/junit.xml
 source venv/bin/activate
+./.lint.sh
 CONFIG_FILE=test python -m pytest --junitxml build/junit.xml dashboard/
 # lsh@2022-04-01: added '--force' flag to ignore warnings until someone can fix the warnings.
 # lsh@2022-08-22: added 'TZ=UTC' to force headless browser in to UTC. tests with naive datetimes now pass.
