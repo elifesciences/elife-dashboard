3ec22080546160cfb53c1762d1a1067c9e70ea5c
Luke <lsh-0@users.noreply.github.com>
2023-03-03 14:45:21 +1030

    project-tests.sh, removes mention of CONFIG_FILE

--- project_tests.sh
+++ project_tests.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -e
 ./install.sh
-if [ ! -e settings_test.py ]; then
-    ln -sf dashboard/dev_settings_test.py settings_test.py
-fi
 rm -rf build/junit.xml
 source venv/bin/activate
 ./.lint.sh
-CONFIG_FILE=test python -m pytest --junitxml build/junit.xml dashboard/
+python -m pytest --junitxml build/junit.xml dashboard/
 # lsh@2022-04-01: added '--force' flag to ignore warnings until someone can fix the warnings.
 # lsh@2022-08-22: added 'TZ=UTC' to force headless browser in to UTC. tests with naive datetimes now pass.
 # broke when 'pickadate' upgraded to 3.5.6. change introduced in 3.5.5 (patch version)
