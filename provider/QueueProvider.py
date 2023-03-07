7301e556f00009b607a1209388db9ff2da0a3d71
Luke <lsh-0@users.noreply.github.com>
2023-03-07 10:50:57 +1030

    typo

--- provider/QueueProvider.py
+++ provider/QueueProvider.py
@@ -4,7 +4,7 @@ from config_decider import config as settings
 class QueueProvider:
 
     def get_queue(self, queue_name):
-        sqs_conn = boto3.resource('sqs', region_name=region)
+        sqs_conn = boto3.resource('sqs', region_name=settings.sqs_region)
         queue = sqs_conn.get_queue_by_name(queue_name)
         return queue
 
