6c34db4a6984a9321202ed9d88cd26eddb0c40f6
Luke <lsh-0@users.noreply.github.com>
2023-03-07 11:06:02 +1030

    queueprovider.py, boto3 fixes

--- provider/QueueProvider.py
+++ provider/QueueProvider.py
@@ -1,10 +1,17 @@
 import boto3
 from config_decider import config as settings
+from botocore.exceptions import ClientError
+import logging
+
+LOG = logging.getLogger(__name__)
 
 class QueueProvider:
 
     def get_queue(self, queue_name):
         sqs_conn = boto3.resource('sqs', region_name=settings.sqs_region)
-        queue = sqs_conn.get_queue_by_name(queue_name)
-        return queue
-
+        try:
+            queue = sqs_conn.get_queue_by_name(QueueName=queue_name)
+            return queue
+        except ClientError as error:
+            LOG.error("failed to fetch queue %r in region %r", queue_name, settings.sqs_region)
+            return None
