b6008ef77a1cdd5d0c8efb6dcfcb8bd84e7178d8
LinaKind <l.kinduryte@elifesciences.org>
2026-03-09 15:59:14 +0200

    Fix hostname in ci
    https://github.com/elifesciences/issues/issues/9497

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -3,7 +3,12 @@ set -ex
 
 #elife-dashboard
 #WARNING: this should be enabled and pulling credentials from somewhere!
-[ $(curl --write-out %{http_code} --silent --output /dev/null https://$(hostname)/api/current) == 200 ]
+hostname=$(hostname)
+if [[ "$hostname" == "ci--ppp-dash.elifesciences.org" ]]; then
+    hostname="ci-ppp-dash.elifesciences.org"
+fi
+
+[ $(curl --write-out %{http_code} --silent --output /dev/null https://$hostname/api/current) == 200 ]
 
 # elife-article-scheduler
 [ $(curl --write-out %{http_code} --silent --output /dev/null http://localhost:8080/schedule/ping) == 200 ]
