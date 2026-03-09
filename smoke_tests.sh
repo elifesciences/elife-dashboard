47a6b74eb159f31627c0007b88b12f81079af7ef
LinaKind <l.kinduryte@elifesciences.org>
2026-03-09 15:27:07 +0200

    Reenable smoke test
    https://github.com/elifesciences/issues/issues/9497

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 set -ex
 
-# elife-dashboard
-# WARNING: this should be enabled and pulling credentials from somewhere!
-#[ $(curl --write-out %{http_code} --silent --output /dev/null https://$(hostname)/api/current) == 200 ]
-
-# elife-dashboard2
-#[ $(curl --write-out %{http_code} --silent --output /dev/null https://$(hostname):8081/api/current) == 200 ]
+#elife-dashboard
+#WARNING: this should be enabled and pulling credentials from somewhere!
+[ $(curl --write-out %{http_code} --silent --output /dev/null https://$(hostname)/api/current) == 200 ]
 
 # elife-article-scheduler
 [ $(curl --write-out %{http_code} --silent --output /dev/null http://localhost:8080/schedule/ping) == 200 ]
